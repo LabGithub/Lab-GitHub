85c886e835fb2ebdd7cb8da3e790c586037379dc
Norah <Norahmutairi@outlook.sa>
2021-06-27 11:56:19 +0300

    add mvc project

---
+++ GitLab/Controllers/ProductsController.cs
@@ -0,0 +1,38 @@
+﻿using GreenLifeStore.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitLab.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ProductsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var Products = _db.Products.ToList();
+            ViewData["Products"] = Products;
+            return View();
+        }
+        // GET: /products/id
+        public IActionResult Details(int? id)
+        {
+            var Product = _db.Products.ToList().Find(product => product.ProductId == id);
+            if (id == null || Product == null)
+            {
+                return View("_NotFound");
+            }
+            ViewData["Product"] = Product;
+            return View(Product);
+
+        }
+
+    }
+}
