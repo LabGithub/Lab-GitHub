49371982e3194d111de2e4276a1eb6ff9656a7c4
teefeid <taef_415@hotmail.com>
2021-06-28 10:37:08 +0300

    Edit home

--- GitLab/Controllers/HomeController.cs
+++ GitLab/Controllers/HomeController.cs
@@ -20,7 +20,7 @@ namespace GitLab.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return View("Index");
         }
 
         public IActionResult Privacy()
